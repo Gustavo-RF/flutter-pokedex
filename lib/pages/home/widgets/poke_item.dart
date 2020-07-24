2d628f1c9942d43cce129b1c9035132318bb975b
Gustavo R <gustavoramaldes@gmail.com>
2020-07-24 14:38:23 -0300

    Showing types

--- lib/pages/home/widgets/poke_item.dart
+++ lib/pages/home/widgets/poke_item.dart
@@ -10,6 +10,45 @@ class PokeItem extends StatelessWidget {
   final String num;
   final List<String> types; 
 
+  Widget setTypes() {
+    List<Widget> list = [];
+    types.forEach((name) { 
+      list.add(
+        Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(24.0),
+                color: Color.fromARGB(80, 255, 255, 255),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  name.trim(),
+                  style: TextStyle(
+                    fontFamily: 'Google',
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white, 
+                  )
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            )
+          ],
+        )
+      );
+    });
+
+    return Column(
+      children: list,
+      crossAxisAlignment: CrossAxisAlignment.start,
+    );
+  }
+
   const PokeItem({Key key, this.name, this.index, this.color, this.num, this.types}) : super(key: key);
 
   @override
@@ -26,38 +65,56 @@ class PokeItem extends StatelessWidget {
           child: Stack(
             // alignment: Alignment.bottomRight,
             children: <Widget> [
+              
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Pokemon name
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      name,
+                      style: TextStyle(
+                        fontFamily: 'Google',
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white
+                      ),
+                    ),
+                  ),
+
+                  // Pokemon types
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0, top: 8.0),
+                    child: setTypes(),
+                  ),
+                ],
+              ),
+
+
+              // Pokeball behind pokemon image
               Align(
                 alignment: Alignment.bottomRight,
                 child: Opacity(
                   opacity: 0.2,
                     child: Image.asset(
                     ConstsApp.whitePokeball,
-                    height: 96.0,
-                    width: 96.0,
+                    height: 80.0,
+                    width: 80.0,
                   ),
                 ),
               ),
+
+              // Pokemon image
               Align(
                 alignment: Alignment.bottomRight,
                 child: CachedNetworkImage(
-                  height: 96.0,
-                  width: 96.0,
+                  height: 80.0,
+                  width: 80.0,
                   placeholder: (context, uri) => new Container( color: Colors.transparent,),
                   imageUrl: 'https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/$num.png',
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  name,
-                  style: TextStyle(
-                    fontFamily: 'Google',
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white
-                  ),
-                ),
-              )
             ]
           ),
         ),
